92d916832f4261433fedef6da1f49434c06d6ada
songyahui <31238748+songyahui@users.noreply.github.com>
2020-01-11 18:40:01 +0800

    m

---
+++ Lib/RegToNfa.ml
@@ -0,0 +1,57 @@
+(** Top-level program for building DOT representations of NFAs from regexes *)
+open Nfa
+open  Int32
+open Set
+
+let die fmt = Printf.kprintf (fun s -> prerr_endline s; exit 1) fmt
+
+let typ = ref "nfa"
+
+let spec = 
+  [("-type",
+    Arg.Symbol (["nfa"; "dfa"; "dfa-minimized"],
+                (:=) typ),
+    "Output type")]
+
+let usage = "re-nfa [options] regex"
+
+let parse_re r = 
+  try Regex.parse r
+  with Regex.Parse_error s -> die "Invalid regular expression: %S\n" s
+
+let () =
+  print_string ("\n");
+  let lhs = "(((c|c)cb*)|((ba|aa)*))(((bc*)|((b|b)|(b*)))|(((a|a)*)|((b*)*)))" in 
+  let rhs = "(((c|c)cb*)|((ba|aa)*))(((bc*)|((b|b)|(b*)))|(((a|a)*)|((b*)*)))" in 
+  let nfaA = Regex.compile (parse_re lhs) in
+  let nfaB = Regex.compile (parse_re rhs) in
+  (*print_string (string_of_bool (Nfa.accept nfa ['a';'c']));*) 
+  (*StateSet.iter (fun a  -> print_string (Int32.to_string a) ) nfa.finals*)
+  print_string(string_of_bool (Nfa.antichain nfaA nfaB [])^"\n")
+  
+  ;;
+  (*
+  let r = ref None in
+  let collect s =
+    match !r with None -> r := Some s
+                | Some _ -> ()
+  in
+  Arg.parse spec collect usage;
+  match !r, !typ with
+  | None, _ -> ()
+  | Some r, "nfa" -> 
+     let nfa = Regex.compile (parse_re r) in
+     let digraph = Nfa_dot.digraph_of_nfa nfa in
+     Format.printf "%a@." Nfa_dot.format_digraph digraph;
+  | Some r, "dfa" ->
+     let nfa = Regex.compile (parse_re r) in
+     let dfa = Dfa.determinize nfa in
+     let digraph = Nfa_dot.digraph_of_nfa (Dfa.inject dfa) in
+     Format.printf "%a@." Nfa_dot.format_digraph digraph;
+  | Some r, "dfa-minimized" ->
+     let nfa = Regex.compile (parse_re r) in
+     let dfa = Dfa.minimize (Dfa.determinize nfa) in
+     let digraph = Nfa_dot.digraph_of_nfa (Dfa.inject dfa) in
+     Format.printf "%a@." Nfa_dot.format_digraph digraph;
+  | _ -> Arg.usage spec usage
+*)
