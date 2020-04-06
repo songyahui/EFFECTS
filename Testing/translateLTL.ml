2ffd0a2fc29ca7b0c7ed62c885ae403124c28763
songyahui <31238748+songyahui@users.noreply.github.com>
2020-04-06 15:58:47 +0800

    m

--- Testing/translateLTL.ml
+++ Testing/translateLTL.ml
@@ -3,6 +3,7 @@ open Ast
 open Pretty
 open Printf
 open List
+open String
 
 let test = "[]<>green;\n"^ "[] (red ->(! X green)) ;\n"^"[] (red -> (<> green));\n"^"(red -> (! (green U yellow))); \n" ^"[] (red -> ((<> green) && (! (green U yellow)))); \n" ^"(red-> X (red U (yellow && X (yellow U green))));\n" ;;
 
@@ -19,16 +20,55 @@ let rec showLTL (ltl:ltl):string =
   | AndLTL (l1, l2) -> "(" ^showLTL l1 ^ " && " ^showLTL l2 ^")"
   ;;
 
-let rec translateLTL (ltl:ltl) :es =
+(*used to generate the free veriables, for subsititution*)
+let freeVar = ["t1"; "t2"; "t3"; "t4";"t5";"t6";"t7";"t8";"t9";"t10"
+              ;"t11"; "t12"; "t13"; "t14";"t15";"t16";"t17";"t18";"t19";"t20"
+              ;"t21"; "t22"; "t23"; "t24";"t25";"t26";"t27";"t28";"t29";"t30"];;
+
+
+
+let rec getAfreeVar (varList:string list):string  =
+  let rec findOne li = 
+    match li with 
+        [] -> raise ( Foo "freeVar list too small exception!")
+      | x :: xs -> if (exists (fun a -> String.compare a x == 0) varList) == true then findOne xs else x
+  in
+  findOne freeVar
+;;
+
+let rec translateLTL (ltl:ltl) (varList:string list) :(es * string list) =
   match ltl with 
-    Lable str -> Event (str, None)
-  | Next l -> Cons (Underline, translateLTL l) 
-  | Until (l1, l2) -> Cons (Kleene (translateLTL l1), translateLTL l2)
-  | Global l -> Omega (translateLTL l)
-  | Future l -> Cons (Kleene (Underline), translateLTL l)
-  | NotLTL l -> Not (translateLTL l)
-  | Imply (l1, l2) -> ESOr (Not (translateLTL l1), ESAnd (translateLTL l1, translateLTL l2))
-  | AndLTL (l1, l2) -> ESAnd (translateLTL l1, translateLTL l2)
+    Lable str -> (Event (str, None), varList)
+  | Next l -> 
+    let (ess, varList') =  translateLTL l varList in 
+    (Cons (Underline, ess), varList')
+  | Until (l1, l2) -> 
+      let newVar = getAfreeVar varList in 
+      let (ess1, varList1) =  translateLTL l1 varList in 
+      let (ess2, varList2) =  translateLTL l2 varList1 in 
+      let prefix = Ttimes (ess2, Var newVar) in 
+      (Cons (prefix, ess2), varList2)
+  | Global l -> 
+      let (ess, varList') =  translateLTL l varList in 
+      (Omega (ess), varList')
+  | Future l -> 
+      let newVar = getAfreeVar varList in 
+      let prefix = Ttimes (Underline, Var newVar) in 
+      let (ess, varList') =  translateLTL l (newVar::varList) in 
+      (Cons (prefix, ess), varList')
+  | NotLTL l -> 
+      let (ess, varList') =  translateLTL l varList in 
+      (Not (ess), varList')
+  | Imply (l1, l2) -> 
+      let (ess1, varList1) =  translateLTL l1 varList in 
+      let (ess2, varList2) =  translateLTL l2 varList1 in 
+      (ESOr (Not (ess1), ESAnd (ess1, ess2)), varList2)
+  | AndLTL (l1, l2) -> 
+      let (ess1, varList1) =  translateLTL l1 varList in 
+      let (ess2, varList2) =  translateLTL l2 varList1 in 
+  
+      (ESAnd (ess1, ess2), varList2)
+
   ;;
 
 let rec input_lines file =
@@ -37,6 +77,8 @@ let rec input_lines file =
   | [line] -> (String.trim line) :: input_lines file
   | _ -> failwith "Weird input_line return value"
 
+
+let get_0 (a,_) = a ;;
 
 let main = 
   let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in 
@@ -47,7 +89,7 @@ let main =
     let lines = List.fold_right (fun x acc -> acc ^ "\n" ^ x) ( specs) "" in 
     let ltlList:(ltl list) = Parser.ltl_p Lexer.token (Lexing.from_string  lines)  in
 
-    let esList=List.map (fun ltl-> translateLTL ltl) (ltlList)  in 
+    let esList=List.map (fun ltl-> get_0 (translateLTL ltl []) ) (ltlList)  in 
     let producte = List.combine ltlList esList in
     let result = List.fold_right (fun (l,e) acc -> acc ^ showLTL l ^ " ==> "^(showES e) ^"\n\n") (producte)  "" in 
     let oc = open_out outputfile in    (* 新建或修改文件,返回通道 *)
