2a526fe2a41a0fb9d1befbdeb15668c289228542
songyahui <31238748+songyahui@users.noreply.github.com>
2020-05-08 19:00:10 +0800

    m

--- Verifier/Forward.ml
+++ Verifier/Forward.ml
@@ -356,33 +356,36 @@ let rec getIncludedFiles (p:(bool * declare) list) :(bool * declare) list =
   appendUp;;
 
 
-let () = 
-    let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in 
-    let outputfile = (Sys.getcwd ()^ "/" ^ Sys.argv.(2)) in
-    let ic = open_in inputfile in
-    try 
-      let lines =  (input_lines ic ) in  
-      let line = List.fold_right (fun x acc -> acc ^ "\n" ^ x) (List.rev lines) "" in 
+let () =
+  let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in
+(*    let outputfile = (Sys.getcwd ()^ "/" ^ Sys.argv.(2)) in
+print_string (inputfile ^ "\n" ^ outputfile^"\n");*)
+  let ic = open_in inputfile in
+  try
+      let lines =  (input_lines ic ) in
+      let line = List.fold_right (fun x acc -> acc ^ "\n" ^ x) (List.rev lines) "" in
       let raw_prog = List.map (fun a -> (true, a)) (Parser.prog Lexer.token (Lexing.from_string line)) in
-      let prog = getIncludedFiles raw_prog in 
-      
+      let prog = getIncludedFiles raw_prog in
+
       (*
       let testprintProg = printProg prog in 
       print_string testprintProg;
 *)
-      let evn = List.map (fun (ind, a) -> a) prog in 
+      let evn = List.map (fun (ind, a) -> a) prog in
       let verification_re = List.fold_right (fun dec acc -> acc ^ (verification dec evn)) prog ""  in
-      let oc = open_out outputfile in    (* 新建或修改文件,返回通道 *)
+      (*let oc = open_out outputfile in    (* 新建或修改文件,返回通道 *)
       (*      let startTimeStamp = Sys.time() in*)
-      fprintf oc "%s\n" verification_re;   (* 写一些东西 *)
+      (*fprintf oc "%s\n" verification_re;   (* 写一些东西 *)*)
+      print_string (verification_re ^"\n");
       (*print_string (string_of_float(Sys.time() -. startTimeStamp)^"\n" );*)
       close_out oc;                (* 写入并关闭通道 *)
-      
+      *)
+      print_string (verification_re ^"\n");
       flush stdout;                (* 现在写入默认设备 *)
-      close_in ic                  (* 关闭输入通道 *) 
-  
+      close_in ic                  (* 关闭输入通道 *)
+
     with e ->                      (* 一些不可预见的异常发生 *)
       close_in_noerr ic;           (* 紧急关闭 *)
       raise e                      (* 以出错的形式退出: 文件已关闭,但通道没有写入东西 *)
-  
+
    ;;
